1e761e567694772a232ace95570b606d2260e3d5
sumit sinha <sumit31aug20022gmail.com>
2026-05-16 13:47:15 +0530

    Updated production API URL

--- frontend/src/api/dashboardApi.js
+++ frontend/src/api/dashboardApi.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL =
+  "https://blackcoffer-dashboard-yj0y.onrender.com/api";
 
 export const fetchInsightsData = async () => {
-  const response = await axios.get(`${API_BASE_URL}/insights/`);
+
+  const response = await axios.get(
+    `${BASE_URL}/insights/`
+  );
+
   return response.data;
 };
 
 export const fetchFilterOptions = async () => {
-  const response = await axios.get(`${API_BASE_URL}/filters/`);
+
+  const response = await axios.get(
+    `${BASE_URL}/filters/`
+  );
+
   return response.data;
 };
